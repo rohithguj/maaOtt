ac3ea931e583c2a8203d88ddaf32261e6eddb24c
G Sree Rohith Chowdary <109966448+rohithguj@users.noreply.github.com>
2024-06-24 19:29:35 +0530

    new local env for secrets

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,13 +9,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAetQ3F6SuxdG2UQLHbkTFBEys-I8qbeqg",
-  authDomain: "maaott-6a7b1.firebaseapp.com",
-  projectId: "maaott-6a7b1",
-  storageBucket: "maaott-6a7b1.appspot.com",
-  messagingSenderId: "250690139816",
-  appId: "1:250690139816:web:f80ff76f43523d06edf49d",
-  measurementId: "G-Z9CR55DKC4"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
